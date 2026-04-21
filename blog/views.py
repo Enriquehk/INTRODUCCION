286b1da42e2453a486f02d42af4ac38f71e78976
kike <jesuscastillo51294@gmail.com>
2026-04-20 22:34:34 -0400

    agregando las vistas del blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View
 
 # Create your views here.
+
+class BlogListView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'blog_list.html')
